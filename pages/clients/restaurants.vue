cfcf7a6ac6884e71f86fb5bc32c56b5b85c039ec
Francis Gyimah <fgyimah883@gmail.com>
2021-05-14 21:47:54 +0000

    Prepared restaurants skeleton

---
+++ pages/clients/restaurants.vue
@@ -0,0 +1,38 @@
+<script>
+/**
+ * Restaurants component
+ */
+export default {
+  head() {
+    return {
+      title: `Client Restaurants | BinBudget`
+    };
+  },
+  data() {
+    return {
+      title: "Restaurants Page",
+      items: [
+        {
+          text: "Binbudget"
+        },
+        {
+          text: "Clients Dashboard"
+        },
+        {
+          text: "Restaurants Page",
+          active: true
+        }
+      ]
+    };
+  },
+  middleware: "authentication"
+};
+</script>
+
+<template>
+  <div>
+    <PageHeader :title="title" :items="items" />
+  </div>
+</template>
+
+<style></style>
