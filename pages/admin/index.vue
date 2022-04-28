5abddc1b810ed5161165bbcbd41cc810012fcf1a
Mohamed Amine Berroug <ma.berroug@aggrom.com>
2022-04-28 18:04:41 +0000

    TEST: added log reder in dmin

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -2,7 +2,9 @@
 /**
  * Dashboard component
  */
+const fs = require('fs');
 export default {
+  
   head() {
     return {
       title: `Tableau de bord | BinBudget`,
@@ -11,9 +13,20 @@ export default {
   methods: {
    
   },
+  mounted(){
+    fs.readFile('/root/.pm2/logs/strapi-out.log', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  this.logs=data
+  console.log(data);
+});
+  },
   data() {
     return {
       title: "Tableau de bord",
+      logs:null,
       items: [
         {
           text: "Binbudget",
@@ -36,6 +49,7 @@ export default {
       
       <h3 class="m-5">En cours</h3>
       <p class="m-5">En cours de test.</p>
+      <p>{{logs}}</p>
     </center>
   </div>
 </template>
