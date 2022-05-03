ea6bca356754e5598ee7fcc33949cd10acf40102
Mohamed Amine Berroug <ma.berroug@aggrom.com>
2022-05-03 19:19:09 +0000

    TEST: deleted test mode

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -2,7 +2,7 @@
 /**
  * Dashboard component
  */
-const fs = require('fs');
+
 export default {
   
   head() {
@@ -14,19 +14,12 @@ export default {
    
   },
   mounted(){
-    fs.readFile('/root/.pm2/logs/strapi-out.log', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  this.logs=data
-  console.log(data);
-});
+   
   },
   data() {
     return {
       title: "Tableau de bord",
-      logs:null,
+
       items: [
         {
           text: "Binbudget",
@@ -48,8 +41,8 @@ export default {
     <center>
       
       <h3 class="m-5">En cours</h3>
-      <p class="m-5">En cours de test.</p>
-      <p>{{logs}}</p>
+      
+
     </center>
   </div>
 </template>
